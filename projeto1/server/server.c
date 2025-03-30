333c7567a3cd5a6de20bbbac23d9e92724cfccfd
dansc00 <danielsousa.cipriano@gmail.com>
2025-03-30 02:14:07 -0300

    Inicio da implementação do servidor echo de teste

--- projeto1/server/server.c
+++ projeto1/server/server.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char **argv)
+{
 
-    printf("teste servidor");
+    // descritor de arquivo: inteiro que referencia um recurso aberto para o kernel
+    int sock, new_sock; // descritores de arquivo para processo pai e filho
+    pid_t child_pid; // pid do processo filho
+
+    // socklen_t: inteiro de pelo menos 32 bits, utilizado para manter um padrão de tamanho, independente da arquitetura do sistema. Garante portabilidade e atendimento do padrão posix
+    socklen_t client_len; // armazena tamanho em bytes de estruturas de endereço socket
+
+    struct sockaddr_in client_addr, server_addr; // estrutura que armazena parâmetros para criação de socket
+
+    // cria socket TCP, usando IPv4 e protocolo padrão
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock == -1){
+        perror("Falha ao criar o socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr)); // zera bloco de memória, evita bugs ao utilizar bind(), connect() 
+
+    
 }
