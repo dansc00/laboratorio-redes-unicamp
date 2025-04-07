e9f893c6ee57528396b181aba7e62d42e58ee085
dansc00 <danielsousa.cipriano@gmail.com>
2025-04-07 20:03:58 -0300

    Fim da implementação do cliente echo

--- projeto1/client/client.c
+++ projeto1/client/client.c
@@ -1,7 +1,145 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h> // funções de socket
+#include <unistd.h> // read(), write(), close() ...
+#include <netinet/in.h> // sockaddr_in, htons(), INADDR_ANY ...
+#include <string.h>
+#include <arpa/inet.h> // conversões de IP como inet_pton()
+#include <errno.h>
 
-int main(){
+#define SERVER_PORT 8080 // porta de escuta do servidor
+#define MAX_PAYLOAD 1024 // tamanho máximo do payload
 
-    printf("teste cliente");
+/*
+estruturas, tipos e constantes definidas em <netinet/in.h>
+
+INADDR_ANY; possui valor 0x00000000, usado para referir todas interfaces de rede 0.0.0.0
+
+in_port_t; equivalente ao tipo uint16_t (16 bits unsigned int)
+
+in_addr_t; equivalente ao tipo uint32_t (32 bits unsigned int)
+
+struct in_addr{
+
+    in_addr_t s_addr; representa o IP
+}
+
+struct sockaddr_in{
+
+    sa_family_t sin_family; familia de endereçamento IP (unsigned int) 
+    in_port_t sin_port; número de porta
+    struct in_addr sin_addr; endereço ip
+}
+*/
+
+// lê um caractere por vez do socket até encontrar \n ou EOF, retorna número de bytes lidos, 0 se EOF ou -1 se houver erro
+ssize_t read_line(int file_descriptor, void *ptr_buffer, size_t max_len) {
+
+    ssize_t n; // número total de bytes lidos
+    ssize_t rc; // retorno de read (bytes lidos)
+    char c; // caractere lido do socket
+    char *ptr; // caminha no buffer
+
+    ptr = ptr_buffer;
+    for(n = 1; n < max_len; n++) {
+
+        if((rc = read(file_descriptor, &c, 1)) == 1) { // byte lido
+            *ptr= c; // adiciona caractere
+            ptr++; // passa para a próxima posição
+
+            if(c == '\n')
+                break;  // fim da linha
+        } 
+        else if(rc == 0) {
+
+            if(n == 1)
+                return 0;  // EOF, nada lido
+            else
+                break;     // EOF, retornando dados lidos
+        } 
+        else{
+            if(errno == EINTR) // sinal de interrupção de função
+                continue;  // tenta novamente
+            
+            // falha real
+            perror("Falha de escrita");
+            return EXIT_FAILURE;             
+        }
+    }
+
+    *ptr = 0;  // \0 termina a string
+    return n;
+}
+
+// envia todos os bytes necessários repetindo write, retorna número de bytes escritos
+ssize_t write_all(int file_descriptor, void *ptr_buffer, size_t n){
+
+    size_t n_left = n; // bytes restantes para envio
+    const char *ptr = ptr_buffer; // ponteiro char aponta para o buffer, se utiliza char que possui tamanho de 1 byte para manipulação de byte por byte
+    ssize_t n_written; // bytes enviados
+
+    while(n_left > 0){
+        // função de escrita, retorna quantidade de bytes escritos (pode n enviar todos de uma vez), 0 (raro) ou -1 caso ocorra um erro
+        n_written = write(file_descriptor, ptr, n_left);
+        if(n_written <= 0){
+
+            if(n_written < 0 && errno == EINTR) // sinal de interrupção de função
+                n_written = 0; // reinicia
+            else{
+                perror("Falha de leitura"); // falha real
+                return EXIT_FAILURE;
+            } 
+        }
+        n_left -= n_written;
+        ptr += n_written;
+    }// falha real
+    return n;
+}
+
+// recebe arquivo de entrada (stdin), retorna e envia ao servidor
+void client_echo(FILE *fp, int sock){
+
+    char send_buffer[MAX_PAYLOAD]; //  buffer de envio de dados
+    char recv_buffer[MAX_PAYLOAD]; // buffer de recebimento de dados
+    
+    // lê linhas do arquivo e armazena no buffer de envio
+    while(fgets(send_buffer, MAX_PAYLOAD, fp) != NULL){
+        write_all(sock, send_buffer, strlen(send_buffer)); // escreve no servidor
+        
+        if(read_line(sock, recv_buffer, MAX_PAYLOAD) == 0){
+            perror("O servidor encerrou a conexão prematuramente");
+            exit(EXIT_FAILURE);
+        }
+
+        fputs(recv_buffer, stdout);
+    }
+}
+
+int main(int argc, char **argv){
+
+    int sock;
+    struct sockaddr_in server_addr; 
+
+    if(argc != 2){ // IP do servidor não foi passado por linha de comando
+        perror("IP do servidor não especificado");
+        exit(EXIT_FAILURE);
+    }
+
+    // cria socket TCP, usando IPv4 e protocolo padrão
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    // zera bloco de memória, evita bugs ao utilizar bind(), connect() 
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET; //IPv4
+    // htonx(); funções htonx(host to network x) convertem ordem de bytes x do host para a ordem de bytes da rede
+    // ex: host usa little-endian e rede usa big-endian (ordem padrão na rede)
+    server_addr.sin_port = htons(SERVER_PORT);
+    // converte IP passado de string para formato binário e armazena no campo correto da struct
+    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+
+    connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); // estabele conexão com o servidor
+    client_echo(stdin, sock);
+    exit(EXIT_SUCCESS);
 }
--- projeto1/server/server.c
+++ projeto1/server/server.c
@@ -8,7 +8,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
-#define SERV_PORT 8080 // porta de escuta do servidor
+#define SERVER_PORT 8080 // porta de escuta do servidor
 #define BACKLOG 5 // número de conexões que podem aguardar na fila de espera
 #define MAX_PAYLOAD 1024 // tamanho máximo do payload
 
@@ -34,7 +34,46 @@ struct sockaddr_in{
 }
 */
 
-// envia todos os bytes necessários repetindo write
+// lê um caractere por vez do socket até encontrar \n ou EOF, retorna número de bytes lidos, 0 se EOF ou -1 se houver erro
+ssize_t read_line(int file_descriptor, void *ptr_buffer, size_t max_len) {
+
+    ssize_t n; // número total de bytes lidos
+    ssize_t rc; // retorno de read (bytes lidos)
+    char c; // caractere lido do socket
+    char *ptr; // caminha no buffer
+
+    ptr = ptr_buffer;
+    for(n = 1; n < max_len; n++) {
+
+        if((rc = read(file_descriptor, &c, 1)) == 1) { // byte lido
+            *ptr= c; // adiciona caractere
+            ptr++; // passa para a próxima posição
+
+            if(c == '\n')
+                break;  // fim da linha
+        } 
+        else if(rc == 0) {
+
+            if(n == 1)
+                return 0;  // EOF, nada lido
+            else
+                break;     // EOF, retornando dados lidos
+        } 
+        else{
+            if(errno == EINTR) // sinal de interrupção de função
+                continue;  // tenta novamente
+            
+            // falha real
+            perror("Falha de escrita");
+            exit(EXIT_FAILURE);             
+        }
+    }
+
+    *ptr = 0;  // \0 termina a string
+    return n;
+}
+
+// envia todos os bytes necessários repetindo write, retorna número de bytes escritos
 ssize_t write_all(int file_descriptor, void *ptr_buffer, size_t n){
 
     size_t n_left = n; // bytes restantes para envio
@@ -45,33 +84,45 @@ ssize_t write_all(int file_descriptor, void *ptr_buffer, size_t n){
         // função de escrita, retorna quantidade de bytes escritos (pode n enviar todos de uma vez), 0 (raro) ou -1 caso ocorra um erro
         n_written = write(file_descriptor, ptr, n_left);
         if(n_written <= 0){
-            if(n_written < 0 && errno == EINTR) // falha por interrupção
+
+            if(n_written < 0 && errno == EINTR) // sinal de interrupção de função
                 n_written = 0; // reinicia
-            else
-                return -1; // falha real
+            else{
+                perror("Falha de leitura"); // falha real
+                exit(EXIT_FAILURE);
+            } 
         }
         n_left -= n_written;
         ptr += n_written;
-    }
+    }// falha real
     return n;
 }
 
 // processa mensagem recebida e escreve echo
-void server_echo(int new_sock)
+void server_echo(int sock)
 {
     ssize_t n; // bytes lidos em dados recebidos, ssize_t é usado para contagem de bytes ou indicação de erro
-    char buffer[MAX_PAYLOAD]; // buffer armazena dados recebidos
+    char rcv_buffer[MAX_PAYLOAD]; // buffer armazena dados recebidos
 
-again:
-    // função de leitura, retorna número de bytes lidos em caso de sucesso, 0 em caso de fim de arquivo (EOF) e número negativo caso haja erro
-    n = read(new_sock, buffer, MAX_PAYLOAD); 
-    while(n > 0)
-        write_all(new_sock, buffer, n);
-    
-    if(n < 0 && errno == EINTR) // repete caso erro por interrupção
-        goto again;
-    else if(n < 0)
-        perror("Erro de leitura");
+    while(1){
+
+        n = read_line(sock, rcv_buffer, MAX_PAYLOAD); 
+        if(n > 0)
+            write_all(sock, rcv_buffer, n);
+
+        else if(n == 0){
+            printf("Conexão encerrada pelo cliente\n");
+            break;
+        }
+
+        else{
+            if(errno == EINTR)
+                continue; // repete caso erro por interrupção
+            
+            perror("Erro de leitura");
+            exit(EXIT_FAILURE);
+        }
+    }
 }
 
 int main(int argc, char **argv){
@@ -94,10 +145,10 @@ int main(int argc, char **argv){
     memset(&server_addr, 0, sizeof(server_addr)); // zera bloco de memória, evita bugs ao utilizar bind(), connect() 
 
     server_addr.sin_family = AF_INET; // define familia de endereçamento IPv4
-    // htonl(); funções htonx(host to network x) convertem ordem de bytes x do host para a ordem de bytes da rede
+    // htonx(); funções htonx(host to network x) convertem ordem de bytes x do host para a ordem de bytes da rede
     // ex: host usa little-endian e rede usa big-endian (ordem padrão na rede)
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // endereço de escuta do servidor. 0.0.0.0 escuta em todas as interfaces
-    server_addr.sin_port = htons(SERV_PORT); // porta do servidor
+    server_addr.sin_port = htons(SERVER_PORT); // porta do servidor
 
     bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); // associa socket ao endereço
     listen(sock, BACKLOG); // coloca o socket em modo de escuta
